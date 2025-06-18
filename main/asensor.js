cbcfaf8d6a5ad9cb3e6b80b7ce5829927502142d
Diego Zambrano <dp1073150812@gmail.com>
2025-06-18 16:41:00 -0500

    Upload html and js for the firts version

---
+++ main/asensor.js
@@ -0,0 +1,144 @@
+let pisoAsensor = 1
+let pasajeroAsensor = 0
+let paradasasensor = 0
+let tiempo = 0
+let interval
+let interval2
+class Persona
+{
+    constructor(nombre, pisoInicial, pisoFinal) {
+        this.nombre = nombre;
+        this.pisoInicial = pisoInicial;
+        this.pisoFinal = pisoFinal;
+    }
+}
+const me = new Persona
+(
+    "Yo", 
+    2,
+    10
+);
+const Carla = new Persona
+(
+    "Carla", 
+    3,
+    4
+);
+const Pedro = new Persona
+(
+    "Pedro", 
+    5,
+    1
+);
+const Juana = new Persona
+(
+    "Juana", 
+    8,
+    7
+);
+
+const personas = [me, Carla, Pedro, Juana];
+console.log(personas)
+let personasEnAscensor =[]
+
+const pisos = [1,2,3,4,5,6,7,8,9,10]
+
+function sigoSibuendoOno (){
+    console.log(pisoAsensor)
+    personas.some(persona => {
+        if (persona.pisoInicial != 'Recogido' || pisoAsensor<=persona.pisoFinal){
+            revisionPisosSubida()
+            return true; // Esto rompe el ciclo
+        }    
+        return false; // Continúa iterando
+    });
+}
+function sigoBajandoOno (){
+    console.log(pisoAsensor)
+    personas.some(persona => {
+        if (persona.pisoFinal != 'Entregado'){
+            revisionPisosbajada()
+            return true; // Esto rompe el ciclo
+        }    
+        return false; // Continúa iterando
+    });
+}
+
+function activarIntervalo(){
+     interval = setInterval(sigoSibuendoOno,50)
+}
+function activarIntervalo2(){
+     interval2 = setInterval(sigoBajandoOno,50)
+}
+
+// revisionPisosSubida()
+// revisionPisosbajada()
+
+console.log('Tiempo: '+tiempo)
+
+function revisionPisosSubida(){
+    // for (let i = 0; i < 10; i++) {
+        subirUnPiso()
+        tiempo++
+    //         console.log(paradasasensor)
+    // console.log(personas)
+    // // return pisoAsensor
+    // }
+    console.log('Paradas Ascensor: '+paradasasensor)
+    // console.log(personas)
+    // return pisoAsensor
+}
+function revisionPisosbajada(){
+    // for (let i = 0; i < 10; i++) {
+        bajarUnPiso()
+        tiempo++
+    // }
+    console.log('Paradas Ascensor: '+paradasasensor)
+}
+function subirUnPiso() {
+        personas.forEach(persona => {      
+            if (persona.pisoInicial == pisoAsensor){
+                persona.pisoInicial = 'Recogido'
+                pasajeroAsensor++
+                paradasasensor++
+                personasEnAscensor.push(persona)
+            }
+        });
+        personasEnAscensor.forEach(persona => {
+            if (persona.pisoFinal == pisoAsensor){
+                persona.pisoFinal = 'Entregado'
+                pasajeroAsensor--
+                paradasasensor++
+            }
+        });
+        if (pisoAsensor<10){
+            pisoAsensor++
+        }
+        if (pisoAsensor==10){
+            clearInterval(interval)
+        }
+}
+function revisarPiso(pisoArevisar){
+    if (pisoArevisar==10) {
+       console.log(pisoAsensor)
+        // bajarUnPiso()
+    }
+}
+function bajarUnPiso() {
+    personas.forEach(persona => {
+        if (persona.pisoFinal == pisoAsensor){
+            persona.pisoFinal = 'Entregado'
+            pasajeroAsensor--
+            paradasasensor++
+        }
+        });
+        if (pisoAsensor>1){
+            pisoAsensor--
+        }
+        if (pisoAsensor==1){
+            clearInterval(interval2)
+        }
+}
+function mostrarMensajeFinal(){
+    console.log('Todos los pasajeros fueron entregados')
+}
